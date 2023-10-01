a8864c5f749150d39fec105aa0ecc624ba971634
Jake <jacobmcpeak98@gmail.com>
2023-10-01 17:04:04 -0400

    Minor cleanup, moving int declaration

--- test/main.c
+++ test/main.c
@@ -8,12 +8,11 @@ main(void) {
 	Suite *s = suite_create("Core");
 	SRunner *sr = srunner_create(s);
 
-	int nf = 0;
 	dummy_tcase(s);
 	service_tcase(s);
 
     srunner_run_all(sr, CK_ENV);
-    nf = srunner_ntests_failed(sr);
+    int nf = srunner_ntests_failed(sr);
     srunner_free(sr);
 
     return nf == 0 ? 0 : 1;
